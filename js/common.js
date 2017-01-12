ec7974de3199ec86bc04b74c19126d8d5886fa19
Taras <tarasserhiytschuk@gmail.com>
2017-01-12 22:43:54 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+
+
+	$('.portfolio-item').magnificPopup({
+		type: 'image',
+	  mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+	  zoom: {
+	    enabled: true, // By default it's false, so don't forget to enable it
+
+	    duration: 300, // duration of the effect, in milliseconds
+	    easing: 'ease-in-out', // CSS transition easing function
+
+	    // The "opener" function should return the element from which popup will be zoomed in
+	    // and to which popup will be scaled down
+	    // By defailt it looks for an image tag:
+	    opener: function(openerElement) {
+	      // openerElement is the element on which popup was initialized, in this case its <a> tag
+	      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+	      return openerElement.is('img') ? openerElement : openerElement.find('img');
+	    }
+	  }
+
+	});
+
+	(function( $ ){
+
+		$(function() {
+
+		  $('.rf').each(function(){
+		    // Объявляем переменные (форма и кнопка отправки)
+			var form = $(this),
+		        btn = form.find('.btn_submit');
+
+		    // Добавляем каждому проверяемому полю, указание что поле пустое
+			form.find('.rfield').addClass('empty_field');
+
+		    // Функция проверки полей формы
+		    function checkInput(){
+		      form.find('.rfield').each(function(){
+		        if(($(this).val() != '') && ($(this).val() != 'Message...')){
+		          // Если поле не пустое удаляем класс-указание
+				$(this).removeClass('empty_field');
+		        } else {
+		          // Если поле пустое добавляем класс-указание
+				$(this).addClass('empty_field');
+		        }
+		      });
+		    }
+
+		    // Функция подсветки незаполненных полей
+		    function lightEmpty(){
+		      form.find('.empty_field').css({'border-color':'#d8512d'});
+		      // Через полсекунды удаляем подсветку
+		      setTimeout(function(){
+		        form.find('.empty_field').removeAttr('style');
+		      },1000);
+		    }
+
+		    // Проверка в режиме реального времени
+		    setInterval(function(){
+		      // Запускаем функцию проверки полей на заполненность
+			  checkInput();
+		      // Считаем к-во незаполненных полей
+		      var sizeEmpty = form.find('.empty_field').size();
+		      // Вешаем условие-тригер на кнопку отправки формы
+		      if(sizeEmpty > 0){
+		        if(btn.hasClass('disabled')){
+		          return false
+		        } else {
+		          btn.addClass('disabled')
+		        }
+		      } else {
+		        btn.removeClass('disabled')
+		      }
+		    },500);
+
+		    // Событие клика по кнопке отправить
+		    btn.click(function(){
+		      if($(this).hasClass('disabled')){
+		        // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
+				lightEmpty();
+		        return false
+		      } else {
+		        // Все хорошо, все заполнено, отправляем форму
+		        form.submit();
+		      }
+		    });
+		  });
+		});
+
+	})( jQuery );
+})
