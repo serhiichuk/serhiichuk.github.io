ef62bb14a69c77cb6993042d312d3408b8c2ab27
Taras <tarasserhiytschuk@gmail.com>
2017-09-27 17:09:07 +0300

    form

--- js/common.js
+++ js/common.js
@@ -26,85 +26,21 @@ $(document).ready(function() {
 
 	});
 
-	(function( $ ){
-
-		$(function() {
-
-		  $('.rf').each(function(){
-		    // Объявляем переменные (форма и кнопка отправки)
-			var form = $(this),
-		        btn = form.find('.btn_submit');
-
-		    // Добавляем каждому проверяемому полю, указание что поле пустое
-			form.find('.rfield').addClass('empty_field');
-
-		    // Функция проверки полей формы
-		    function checkInput(){
-		      form.find('.rfield').each(function(){
-		        if(($(this).val() != '') && ($(this).val() != 'Message...')){
-		          // Если поле не пустое удаляем класс-указание
-				$(this).removeClass('empty_field');
-		        } else {
-		          // Если поле пустое добавляем класс-указание
-				$(this).addClass('empty_field');
-		        }
-		      });
-		    }
-
-		    // Функция подсветки незаполненных полей
-		    function lightEmpty(){
-		      form.find('.empty_field').css({'border-color':'#d8512d'});
-		      // Через полсекунды удаляем подсветку
-		      setTimeout(function(){
-		        form.find('.empty_field').removeAttr('style');
-		      },1000);
-		    }
-
-		    // Проверка в режиме реального времени
-		    setInterval(function(){
-		      // Запускаем функцию проверки полей на заполненность
-			  checkInput();
-		      // Считаем к-во незаполненных полей
-		      var sizeEmpty = form.find('.empty_field').size();
-		      // Вешаем условие-тригер на кнопку отправки формы
-		      if(sizeEmpty > 0){
-		        if(btn.hasClass('disabled')){
-		          return false
-		        } else {
-		          btn.addClass('disabled')
-		        }
-		      } else {
-		        btn.removeClass('disabled')
-		      }
-		    },500);
-
-		    // Событие клика по кнопке отправить
-		    btn.click(function(){
-		      if($(this).hasClass('disabled')){
-		        // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
-				lightEmpty();
-		        return false
-		      } else {
-		        // Все хорошо, все заполнено, отправляем форму
-		        form.submit(function () {
-                    var th = $(this);
-                    $.ajax({
-                        type: "POST",
-                        url: "js/mail.php", //Change
-                        data: th.serialize()
-                    }).done(function() {
-                        alert("Thank you!");
-                        setTimeout(function() {
-                            // Done Functions
-                            th.trigger("reset");
-                        }, 1000);
-                    });
-                    return false;
-                });
-		      }
-		    });
-		  });
-		});
-
-	})( jQuery );
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        console.log('submit', $(this));
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "js/mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 })
