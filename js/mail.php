3042b24470d144c972ee0863815e0a4e156b2fc2
Taras <tarasserhiytschuk@gmail.com>
2017-09-27 17:36:01 +0300

    hren

--- js/mail.php
+++ js/mail.php
@@ -49,4 +49,4 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-mail($admin_email, adopt($form_subject), $message, $headers );
+mail('tarasserhiytschuk@gmail.com', 'hren', 'libasik');
